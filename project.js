9dcd417f383ec76728927402cbca8e2f60fa8483
Azfar-Sphere <s.azfar.rahman@gmail.com>
2023-10-05 11:39:43 +0400

    added winnings and game loop

--- project.js
+++ project.js
@@ -112,12 +112,50 @@ const printRows = (rows) => {
 }
 
 const getWinnings = (rows, bet, lines) => {
-    
+    let winnings = 0;
+
+    for (row = 0; row < lines; row++) {
+        const symbols = rows[row];
+        let allSame = true;
+
+        for (const symbol of symbols) {
+            if (symbol != symbols[0]) {
+                allSame = false;
+                break;
+            }
+        }
+
+        if (allSame) {
+            winnings += bet * SYMBOL_VALUES[symbols[0]];
+        }
+    }
+
+    return winnings;
 }
 
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
-const reels = spin();
-const rows = transpose(reels);
-printRows(rows);
+const game = () => {
+
+    let balance = deposit();
+    while(true) {
+        console.log("You have a balance of $" + balance.toString())
+        const numberOfLines = getNumberOfLines();
+        const bet = getBet(balance, numberOfLines);
+        balance -= (bet * numberOfLines);
+        const reels = spin();
+        const rows = transpose(reels);
+        printRows(rows);
+        const winnings = getWinnings(rows, bet, numberOfLines);
+        balance += winnings;
+        console.log("You won, $" + winnings.toString());
+
+        if (balance <= 0) {
+            console.log("You ran out of money!");
+            break;
+        }
+
+        const playAgain = prompt("Do you want to play again? (y/n)? ")
+        if (playAgain != "y") break;
+    }
+}
+
+game();
